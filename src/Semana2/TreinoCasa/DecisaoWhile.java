8f1bf1a30229fb0a09fc846cb11f1b20bd4b1f37
EmanueleMagalhaes <emanuele.lms@gmail.com>
2025-02-23 20:32:34 -0300

    Incluindo while

---
+++ src/Semana2/TreinoCasa/DecisaoWhile.java
@@ -0,0 +1,15 @@
+package Semana2.TreinoCasa;
+
+public class DecisaoWhile {
+    public static void main(String[] args) {
+        int numero;
+
+        numero = 1;
+
+        while (numero <= 5) {
+            System.out.println("Valor: " + numero);
+            numero = numero + 1;
+        }
+    }
+
+}
